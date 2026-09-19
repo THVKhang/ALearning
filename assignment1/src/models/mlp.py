5b3b571da3b31d0f1f4a15984495b35125ee296f
Khoa4444 <khoa.duongdang@hcmut.edu.vn>
2026-09-20 00:20:21 +0700

    mlp model done

--- assignment1/src/models/mlp.py
+++ assignment1/src/models/mlp.py
@@ -1,23 +1,26 @@
-"""MLP: flattened pixels -> hidden ReLU layer(s) with optional dropout -> class logits."""
-
 from math import prod
-
 from torch import nn
 
-
 class MLP(nn.Module):
-    def __init__(self, input_shape=(1, 28, 28), hidden_sizes=(256,), num_classes=10,
-                 dropout=0.0):
+    def __init__(
+        self,
+        input_shape=(1, 28, 28),
+        hidden_sizes=(256,),
+        num_classes=10,
+        dropout=0.0
+    ):
         super().__init__()
         layers = [nn.Flatten()]
-        in_features = prod(input_shape)
-        for hidden in hidden_sizes:
-            layers += [nn.Linear(in_features, hidden), nn.ReLU()]
+        input_size = prod(input_shape)
+        for hidden_size in hidden_sizes:
+            layers.append(nn.Linear(input_size, hidden_size))
+            layers.append(nn.ReLU())
             if dropout > 0:
                 layers.append(nn.Dropout(dropout))
-            in_features = hidden
-        layers.append(nn.Linear(in_features, num_classes))
+            input_size = hidden_size
+        layers.append(nn.Linear(input_size, num_classes))
         self.net = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.net(x)
+    
